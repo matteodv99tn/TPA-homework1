a6f075cc8b1b4e8e916dd696efaefd75a65fc092
matteodv99tn <matteodv99tn@gmail.com>
2021-04-10 18:21:58 +0200

    aggiunta di una prima versione provvisoria della struttura della guida prismatica

--- libreries/guida.h
+++ libreries/guida.h
@@ -2,4 +2,35 @@
 
     #define _GUIDA_PRISMATICA_H_
 
+    /*
+     *  Definzione della struttura per la guida prismatica
+     * 
+     *  @param lunghezza: lunghezza complessiva della barra della guida prismatica
+     *  @param corsa: posizione relativa del glifo rispetto alla posizione iniziale
+     * 
+     *  @param dim_perno_x: dimensione orizzontale del perno mobile
+     *  @param dim_perno_y: dimensione verticale del perno mobile
+     * 
+     *  @param dim_base_x: dimensione orizzontale dei supporti di montaggio
+     *  @param dim_base_y: dimensione verticale dei supporti di montaggio
+     * 
+     *  @param spessore: spessore della guida prismatica
+     *  @param alpha: inclinazione della guida rispetto all'orizzontale
+     */
+    struct GuidaPrismatica {
+        
+        double lunghezza;
+        double corsa;
+        
+        double dim_perno_x;
+        double dim_perno_y;
+        
+        double dim_base_x;
+        double dim_base_y;
+
+        double spessore;
+        double alpha;
+        
+    };
+
 #endif
