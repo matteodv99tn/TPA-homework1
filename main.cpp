675beb45a9dcb1bb49125d612fc0609e05c2d0fd
Matteo <matteodv99tn@gmail.com>
2021-04-11 18:36:09 +0200

    Aggiunto nel main un primo programma con l'istanziamento di una guida e sua distruzione

--- main.cpp
+++ main.cpp
@@ -1,9 +1,23 @@
 #include <iostream>
-#include "guida.h"
+#include "libreries/guida.h"
 
 using namespace std;
 
 int main() {
+
+    GuidaPrismatica * myguida;
+
+    cout << "Inizializzo una guida prismatica di " << endl;
+    cout << "> lunghezza: 50" << endl;
+    cout << "> corsa: 20" << endl;
+    cout << "> dimesione orizzontale di cerniere e guida: 10" << endl;
+    cout << "> dimesione verticale di cerniere e guida: 10" << endl << endl;
+
+    myguida = guida_init(50,20, 10,10);
+
+
+    cout << "Distruggo la struttura e termino il programma" << endl;
+    guida_distruggi( myguida );
     
-    return 0;
+    return EXIT_SUCCESS;
 }
