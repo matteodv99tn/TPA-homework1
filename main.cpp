d327943d11e9b272df9243ecf728e006c6130e76
Matteo <matteodv99tn@gmail.com>
2021-04-22 11:18:05 +0200

    modificato il file main per interagire con le ultime funzioni integrate

--- main.cpp
+++ main.cpp
@@ -13,12 +13,13 @@ int main() {
     cout << "> dimesione orizzontale di cerniere e guida: 10" << endl;
     cout << "> dimesione verticale di cerniere e guida: 10" << endl << endl;
 
-    myguida = guida_init(200,200,50,20, 10, 10);
+    myguida = guida_init(200,200,250, 50, 20, 60);
 
     // string svgcode = guida_to_SVGstring( myguida );
     guida_to_SVG( myguida , "test");
 
-    // cout << svgcode << endl << endl;
+    cout << "Informazioni sulla guida salvata:" << endl;
+    guida_visualizza_info( myguida );
 
     cout << "Distruggo la struttura e termino il programma" << endl;
     guida_distruggi( myguida );
