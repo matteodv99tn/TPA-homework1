7e2d06e3e20851e95a9d530cdcbdcc3b3d9f6705
matteodv99tn <matteodv99tn@gmail.com>
2021-04-24 15:57:29 +0200

    Aggiornato il file main in versione minimale che permette di effettuare solo il rendering in SVG di una guida prismatica semplice

--- main.cpp
+++ main.cpp
@@ -7,23 +7,10 @@ int main() {
 
     GuidaPrismatica * myguida;
 
-    cout << "Inizializzo una guida prismatica di " << endl;
-    cout << "> lunghezza: 50" << endl;
-    cout << "> corsa: 20" << endl;
-    cout << "> dimesione orizzontale di cerniere e guida: 10" << endl;
-    cout << "> dimesione verticale di cerniere e guida: 10" << endl << endl;
+    myguida = guida_init(400,400,500, 150, 50, 70);
 
-    myguida = guida_init(200,200,250, 100, 30, 60);
+    guida_to_SVG(myguida, "../test");
 
-    guida_modifica( myguida );
-
-    guida_to_SVG( myguida , "test");
-
-    cout << "Informazioni sulla guida salvata:" << endl;
-    guida_visualizza_info( myguida );
-
-    cout << "Distruggo la struttura e termino il programma" << endl;
     guida_distruggi( myguida );
-    
     return EXIT_SUCCESS;
 }
