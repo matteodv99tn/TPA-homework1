dcf1574266eb2034fe30c155bdde3708addeed74
Matteo <matteodv99tn@gmail.com>
2021-04-26 11:30:52 +0200

    Aggiunti i primi test sull'inizializzazione della struttura

--- mytest.cpp
+++ mytest.cpp
@@ -4,8 +4,29 @@
 #include "libreries/catch2.hpp"
 #include "libreries/guida.h"
 
-TEST_CASE("PRova", "[test]"){
+TEST_CASE("Inizializzazione corretta di un puntatore a guida prismatica", "[GuidaPrismatica:inizializzazione]"){
 
-    REQUIRE(1 == 1);
+    GuidaPrismatica * guida = guida_init (100, 100, 200, 100, 50, 60);
+
+    REQUIRE( guida != NULL );
+    REQUIRE( guida->alpha == 0);
+    REQUIRE( guida->corsa == 100);
+    REQUIRE( guida->lunghezza == 200);
+    REQUIRE( guida->incastri->dim_x == 50);
+    REQUIRE( guida->incastri->dim_y == 60);
+    REQUIRE( guida->guida->dim_x == 50);
+    REQUIRE( guida->guida->dim_y == 60);
+    REQUIRE( guida->spessore == 20);
+
+    guida_distruggi( guida );
+}
+
+TEST_CASE("Un'inizializzazione sbagliata di una guida prismatica dovrebbe comportare un puntatore nullo", "[GuidaPrismatica:inizializzazione]"){
+
+    GuidaPrismatica * guida = guida_init (100, 100, 200, -10, -10, -10);
+
+    REQUIRE( guida == NULL );
+
+    guida_distruggi( guida );
 
 }
