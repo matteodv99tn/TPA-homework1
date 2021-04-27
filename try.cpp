d25f14acab4459405687bccbb7cf136ce864b281
StefanoPeroceschi <noreply@stefanoperoceschi.it>
2021-04-27 21:48:32 +0200

    Aggiunto file try.cpp come main per la verifica delle funzionalità introdotte

---
+++ try.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <fstream>
+#include "libreries/guida.h"
+#include "libreries/livellaMobile.h"
+#include "libreries/external/Livella.h"
+#include "libreries/external/sistemaAstePerno.h"
+
+int main(){
+
+    GuidaPrismatica* tmp = guida_init(400,500,30,15,200,20);
+    guida_modifica(tmp);
+
+    guida_to_SVG (tmp,"e");
+    guida_distruggi(tmp);
+}
