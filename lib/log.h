0faa31a59ab512bd7fd1a191c7e4b60913f69853
Matteo <matteodv99tn@gmail.com>
2021-06-22 09:11:07 +0200

    Created a custom LOG macro for debug purpouses

---
+++ lib/log.h
@@ -0,0 +1,18 @@
+
+#ifndef _MY_LOG_H_
+#define _MY_LOG_H_
+
+
+    // To see the log massages set to 1
+    // otherwise set to 0
+    #define LOG_MODE 1
+    
+
+    #if LOG_MODE == 1
+        #include <iostream>
+        #define LOG( value ) std::cout << "LOG info: " <<  __FILE__ << "::" << __FUNCTION__ << "(line " << __LINE__ <<"): " << value << endl;
+    #else
+        #define LOG( value )
+    #endif
+
+#endif
